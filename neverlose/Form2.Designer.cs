b0aa5b5d14ebb6380227778a95448be10daf752b
PotatoSUS <89930105+SUSPotato@users.noreply.github.com>
2022-07-11 21:41:08 -0300

    oieeeee

---
+++ neverlose/Form2.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace gamesense
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label3 = new System.Windows.Forms.Label();
+            this.load = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(206, 230);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "You have subscription.";
+            // 
+            // load
+            // 
+            this.load.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("load.BackgroundImage")));
+            this.load.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.load.FlatAppearance.BorderSize = 0;
+            this.load.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.load.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load.Location = new System.Drawing.Point(298, 380);
+            this.load.Margin = new System.Windows.Forms.Padding(2);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(148, 38);
+            this.load.TabIndex = 5;
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            this.load.MouseDown += new System.Windows.Forms.MouseEventHandler(this.load_MouseDown);
+            this.load.MouseUp += new System.Windows.Forms.MouseEventHandler(this.load_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(563, 429);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.label3);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "neverlose.cc";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form2_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ neverlose/Form3.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace gamesense
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.copy_hwid = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // copy_hwid
+            // 
+            this.copy_hwid.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("copy_hwid.BackgroundImage")));
+            this.copy_hwid.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.copy_hwid.FlatAppearance.BorderSize = 0;
+            this.copy_hwid.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.copy_hwid.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.copy_hwid.Location = new System.Drawing.Point(11, 275);
+            this.copy_hwid.Margin = new System.Windows.Forms.Padding(2);
+            this.copy_hwid.Name = "copy_hwid";
+            this.copy_hwid.Size = new System.Drawing.Size(146, 35);
+            this.copy_hwid.TabIndex = 13;
+            this.copy_hwid.Text = "Copy HWID";
+            this.copy_hwid.UseVisualStyleBackColor = true;
+            this.copy_hwid.Click += new System.EventHandler(this.copy_hwid_Click);
+            // 
+            // back
+            // 
+            this.back.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("back.BackgroundImage")));
+            this.back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.back.FlatAppearance.BorderSize = 0;
+            this.back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.back.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.back.Location = new System.Drawing.Point(345, 275);
+            this.back.Margin = new System.Windows.Forms.Padding(2);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(146, 35);
+            this.back.TabIndex = 14;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(502, 321);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.copy_hwid);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loader Panel";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form3_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button copy_hwid;
+        private System.Windows.Forms.Button back;
+    }
+}
